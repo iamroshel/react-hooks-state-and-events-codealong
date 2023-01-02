e4522e95139c9d9595ab7f0da6dcf836baaf28e2
Roshel Esteron <mynameisroshel19@gmail.com>
2023-01-02 18:34:12 +1000

    Completed Lab

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  let [isOn, roshel] = useState(false)
+  function handleClick() {
+    roshel((isOn) => !isOn); 
+  }
+  const color = isOn ? "red" : "white"
+  return <button style = {{background : color}} onClick={handleClick}> {isOn ? "ON" : "OFF"} </button>;
 }
 
 export default Toggle;
